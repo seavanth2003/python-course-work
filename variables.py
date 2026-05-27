aa88c16f847ff1e2a16f3bace1a5f419c86470f9
Nelluri siva Sai Krishna seavanth <seavanthnelluri609@gmail.com>
2026-05-27 17:55:46 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,41 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> myvar=10
+>>> Mvar=10
+>>> myvar@1=10
+SyntaxError: cannot assign to expression here. Maybe you meant '==' instead of '='?
+>>> my_d=10
+>>> dash=10
+>>> Dash=10
+>>> dash
+10
+>>> Dash
+10
+>>> DASH=6
+>>> wi=6
+>>> 
+====================================================== RESTART: C:\Users\HP\OneDrive\Desktop\python-course-work\DAY-2\keywords.py ======================================================
+['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue', 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+35
+>>> a
+Traceback (most recent call last):
+  File "<pyshell#10>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a,b,c=20,30,40
+>>> a
+20
+>>> b
+30
+>>> c
+40
+>>> if var=10
+SyntaxError: invalid syntax. Maybe you meant '==' or ':=' instead of '='?
+>>> _myvar=5
