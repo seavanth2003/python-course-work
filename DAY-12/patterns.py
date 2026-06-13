b31a7ecb403b9b2527dda3f426d501bb52e72b16
Nelluri siva Sai Krishna seavanth <seavanthnelluri609@gmail.com>
2026-06-13 12:56:59 +0530

    Add files via upload

---
+++ DAY-12/patterns.py
@@ -0,0 +1,77 @@
+'''
+n=int(input("Enter n:"))
+m=n//2
+for i in range(n):
+    if i<m:
+        for j in range(i+1):
+            print('*',end='')
+    else:
+        for j in range(n-i):
+            print('*',end='')
+    print()
+    '''
+'''
+n=int(input("Enter n: "))
+m=n//2
+
+for i in range(n):
+    if i<m:
+        print(' '*(m-i),end=' ')
+        print('*'*(i+1),end=' ')
+    else:
+        print(' '*(i-m),end=' ')
+        print('*'*(n-i),end=' ')
+    print()
+    '''
+'''
+n=int(input("Enter n: "))
+m=n//2
+
+for i in range(n):
+    if i<m:
+        print(' '*(m-i)+'*'*(i+1),end=' ')
+    else:
+        print(' '*(i-m)+'*'*(n-i),end=' ') 
+    print()
+    '''
+'''
+n=int(input("Enter n: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1::
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    '''
+
+'''
+n=int(input("Enter n: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1or i==n//2 or j==n//2:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    '''
+'''
+n=int(input("Enter n: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1 or j==i or i+j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    '''
+
+n=int(input("Enter n: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1  or j==0 or j==n-1 or i==n//2 :
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+
---
+++ DAY-12/results.py
@@ -0,0 +1,277 @@
+Python 3.14.5 (tags/v3.14.5:5607950, May 10 2026, 10:43:50) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+
+====================================================== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====================================================
+Enter n:9
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n:6
+*
+**
+***
+***
+**
+*
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+enter n: 9
+*******************************************************************
+
+Warning (from warnings module):
+  File "C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py", line 22
+    print('*',8(n-i),end=' ')
+SyntaxWarning: 'int' object is not callable; perhaps you missed a comma?
+>>> 
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 8
+     *     **    ***   **** Traceback (most recent call last):
+  File "C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py", line 21, in <module>
+    print(' ',*(i-m),end=' ')
+TypeError: Value after * must be an iterable, not int
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 9
+     *     **    ***   ****  *****   ****    ***     **      * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 9
+     *      * *     * * *    * * * *   * * * * *    * * * *     * * *      * *       *  
+
+====================================================== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====================================================
+Enter n: 9
+     *  
+    * *  
+   * * *  
+  * * * *  
+ * * * * *  
+  * * * *  
+   * * *  
+    * *  
+     *  
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 15
+        *  
+       * *  
+      * * *  
+     * * * *  
+    * * * * *  
+   * * * * * *  
+  * * * * * * *  
+ * * * * * * * *  
+  * * * * * * *  
+   * * * * * *  
+    * * * * *  
+     * * * *  
+      * * *  
+       * *  
+        *  
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 15
+ *  
+ * *  
+ * * *  
+ * * * *  
+ * * * * *  
+ * * * * * *  
+ * * * * * * *  
+ * * * * * * * *  
+ * * * * * * *  
+ * * * * * *  
+ * * * * *  
+ * * * *  
+ * * *  
+ * *  
+ *  
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 9
+     * 
+    ** 
+   *** 
+  **** 
+ ***** 
+  **** 
+   *** 
+    ** 
+     * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 20
+           * 
+          ** 
+         *** 
+        **** 
+       ***** 
+      ****** 
+     ******* 
+    ******** 
+   ********* 
+  ********** 
+ ********** 
+  ********* 
+   ******** 
+    ******* 
+     ****** 
+      ***** 
+       **** 
+        *** 
+         ** 
+          * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 15
+       * 
+      ** 
+     *** 
+    **** 
+   ***** 
+  ****** 
+ ******* 
+******** 
+ ******* 
+  ****** 
+   ***** 
+    **** 
+     *** 
+      ** 
+       * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/condstms.py =====
+Enter n: 10
+     * 
+    ** 
+   *** 
+  **** 
+ ***** 
+***** 
+ **** 
+  *** 
+   ** 
+    * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+* * * * * * * * * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 10
+* * * * * * * * * * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+*                 * 
+* * * * * * * * * * 
+
+Warning (from warnings module):
+  File "C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py", line 41
+    if i==0 or i==n-1 or j==0 or j==n-1or i==n//2 or j==n//2:
+SyntaxWarning: invalid decimal literal
+>>> 
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+*       *       * 
+*       *       * 
+*       *       * 
+* * * * * * * * * 
+*       *       * 
+*       *       * 
+*       *       * 
+* * * * * * * * * 
+
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+* *           * * 
+*   *       *   * 
+*     *   *     * 
+*       *       * 
+*     *   *     * 
+*   *       *   * 
+* *           * * 
+* * * * * * * * * 
+>>> 
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+*               * 
+>>> 
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+*               * 
+>>> 
+====== RESTART: C:/Users/HP/OneDrive/Desktop/python-course-work/DAY-12/patterns.py =====
+Enter n: 9
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
+*               * 
+*               * 
+*               * 
+* * * * * * * * * 
